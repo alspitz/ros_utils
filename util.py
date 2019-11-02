fb9b8fd83fc2337dd0ef82dbbebda2332cf99594
Alex Spitzer <aes368@cornell.edu>
2019-11-02 01:31:59 -0400

    Add functionality to tonp and toros

--- util.py
+++ util.py
@@ -5,8 +5,11 @@ from scipy.spatial.transform import Rotation as R
 from geometry_msgs.msg import Vector3
 
 def tonp(obj):
-  if type(obj) == list:
+  if isinstance(obj, list) or isinstance(obj, tuple):
     return np.array([tonp(x) for x in obj])
+
+  if isinstance(obj, int) or isinstance(obj, float):
+    return obj
 
   w = hasattr(obj, 'w')
   x = hasattr(obj, 'x')
@@ -19,16 +22,29 @@ def tonp(obj):
   elif x and y and z:
     return np.array((obj.x, obj.y, obj.z))
 
+  print(obj)
   assert False
 
 def toros(obj):
-  if len(obj.shape) == 2:
+  if len(obj.shape) == 1:
+    if len(obj) == 3:
+      return Vector3(obj[0], obj[1], obj[2])
+  elif len(obj.shape) == 2:
     if obj.shape[1] == 3:
       return [Vector3(x[0], x[1], x[2]) for x in obj]
 
 def odomtostate(odom):
-  return np.hstack((
-    tonp(odom.pose.pose.position),
-    tonp(odom.twist.twist.linear),
-    tonp(odom.pose.pose.orientation).as_euler('ZYX')[::-1],
-    tonp(odom.twist.twist.angular)))
+  if hasattr(odom.pose, 'pose'):
+    return np.hstack((
+      tonp(odom.pose.pose.position),
+      tonp(odom.twist.twist.linear),
+      tonp(odom.pose.pose.orientation).as_euler('ZYX')[::-1],
+      tonp(odom.twist.twist.angular)))
+  else:
+    # Assume it is OdomNoCov
+    return np.hstack((
+      tonp(odom.pose.position),
+      tonp(odom.twist.linear),
+      tonp(odom.pose.orientation).as_euler('ZYX')[::-1],
+      tonp(odom.twist.angular)))
+
