41a853c76f231725d62d5d3e14230be288fe91e3
Alex Spitzer <aes368@cornell.edu>
2020-03-11 16:02:08 -0400

    toros: Now can output Quaternion messages from scipy Rotation objects

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from scipy.spatial.transform import Rotation as R
 
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Quaternion, Vector3
 
 def tonp(obj):
   if isinstance(obj, list) or isinstance(obj, tuple):
@@ -25,13 +25,27 @@ def tonp(obj):
   print(obj)
   assert False
 
+def _tovec3(obj):
+  return Vector3(obj[0], obj[1], obj[2])
+
+def _toquat(obj):
+  q_wlast = obj.as_quat()
+  q_ret = Quaternion()
+  q_ret.x = q_wlast[0]
+  q_ret.y = q_wlast[1]
+  q_ret.z = q_wlast[2]
+  q_ret.w = q_wlast[3]
+  return q_ret
+
 def toros(obj):
-  if len(obj.shape) == 1:
+  if type(obj) is R:
+    return _toquat(obj)
+  elif len(obj.shape) == 1:
     if len(obj) == 3:
-      return Vector3(obj[0], obj[1], obj[2])
+      return _tovec3(obj)
   elif len(obj.shape) == 2:
     if obj.shape[1] == 3:
-      return [Vector3(x[0], x[1], x[2]) for x in obj]
+      return [_tovec3(x) for x in obj]
 
 def odomtostate(odom):
   if hasattr(odom.pose, 'pose'):
