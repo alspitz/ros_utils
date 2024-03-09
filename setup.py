3b140b308027a5b33e708acfddbf65b21934cf2d
Alex Spitzer <aes368@cornell.edu>
2024-03-09 12:36:15 -0800

    Tag as version 0.1.0 and add info to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="ros_utils",
+    version="0.1.0",
     packages=find_packages(),
     author="Alex Spitzer",
     author_email="aes368@cornell.edu",
+    maintainer="Alex Spitzer",
+    url="https://github.com/alspitz/ros_utils",
     license=open("LICENSE", mode="r").read(),
     install_requires=[
       'scipy>=1.2.0',
